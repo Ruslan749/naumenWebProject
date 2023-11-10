e9620d57e37644dd84259e9d556656ac0def06f7
ruslan749 <73393054+Ruslan749@users.noreply.github.com>
2023-11-10 18:56:01 +0500

    Initial commit

---
+++ src/main/java/com/example/naumenwebproject/NaumenWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.naumenwebproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaumenWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaumenWebProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/naumenwebproject/NaumenWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.naumenwebproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaumenWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
