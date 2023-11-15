e7ec4572fab641e080910e8c49b3994c97187231
ruslan749 <73393054+Ruslan749@users.noreply.github.com>
2023-11-16 00:05:19 +0500

    Spring_Security

--- src/main/java/com/example/naumenwebproject/NaumenWebProjectApplication.java
+++ src/main/java/com/example/naumenwebproject/NaumenWebProjectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NaumenWebProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NaumenWebProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NaumenWebProjectApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/example/naumenwebproject/service/RegistrationServes.java
@@ -0,0 +1,28 @@
+package com.example.naumenwebproject.service;
+
+
+import com.example.naumenwebproject.modals.Person;
+import com.example.naumenwebproject.repositories.PeopleRepositories;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class RegistrationServes {
+    private final PeopleRepositories peopleRepositories;
+    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    public RegistrationServes(PeopleRepositories peopleRepositories,
+                              PasswordEncoder passwordEncoder) {
+        this.peopleRepositories = peopleRepositories;
+        this.passwordEncoder = passwordEncoder;
+    }
+    @Transactional
+    public void register(Person person){
+        String encodedPassword = passwordEncoder.encode(person.getPassword()); // шифруем пороль
+        person.setPassword(encodedPassword); // обнавляем пороль на уже зашифрованный
+        person.setRoles("ROLE_USER"); // каждому зарегистрерованому пользователю назначаем роль user
+        peopleRepositories.save(person);
+    }
+}
