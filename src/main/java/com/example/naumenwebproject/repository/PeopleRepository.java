9aa46ccf8e131a300741db5119074eea832ab09d
Vladislav Andreev <vladislav-andreev2000@mail.ru>
2023-12-15 16:43:01 +0500

    Осталось отполировать и дотестировать Person

---
+++ src/main/java/com/example/naumenwebproject/repository/PeopleRepository.java
@@ -0,0 +1,13 @@
+package com.example.naumenwebproject.repository;
+
+
+import com.example.naumenwebproject.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+    Optional<Person> findByUsername(String username);
+}
